17a11c36e1e36312df4450ad266cdce20199ec72
Bastian Rieck <bastian@rieck.me>
2024-06-04 18:57:55 +0200

    First version of conversion script
    Let's see whether we can get this to run in one of the GitHub actions.
    It would be best if it could ensure that our module is installed using
    `pip` but it also should work like this outside the root folder.

---
+++ scripts/convert_manifolds.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# convert_manifolds.sh: This script performs the automated conversion of
+# triangulations of 2-manifolds and 3-manifolds (originally described in
+# mixed lexicographic format) to compressed JSON files.
+#
+# Usage:
+#
+#   $ sh scripts/convert_manifolds.sh
+#
+# This script assumes it runs in the base directory of the package. 
+
+STELLAR_URL=https://zenodo.org/api/records/11474260/files-archive
+
+curl $STELLAR_URL --output data/manifolds.zip
+unzip data/manifolds.zip
+
+cat data/2_manifolds_all.txt      data/2_manifolds_10_all.txt      > data/2_manifolds.txt
+cat data/2_manifolds_all_type.txt data/2_manifolds_10_all_type.txt > data/2_manifolds_type.txt
+cat data/2_manifolds_all_hom.txt  data/2_manifolds_10_all_hom.txt  > data/2_manifolds_hom.txt
+
+echo "Converting 2-manifolds..."
+
+python -m mantra.lex_to_json            data/2_manifolds.txt      \
+                             --type     data/2_manifolds_type.txt \
+                             --homology data/2_manifolds_hom.txt  \
+          > 2_manifolds.json
+
+gzip --best 2_manifolds.json
+
+echo "Converting 3-manifolds..."
+
+cat data/3_manifolds_all.txt      data/3_manifolds_10_all.txt      > data/3_manifolds.txt
+cat data/3_manifolds_all_type.txt data/3_manifolds_10_all_type.txt > data/3_manifolds_type.txt
+cat data/3_manifolds_all_hom.txt  data/3_manifolds_10_all_hom.txt  > data/3_manifolds_hom.txt
+
+python -m mantra.lex_to_json            data/3_manifolds.txt      \
+                             --type     data/3_manifolds_type.txt \
+                             --homology data/3_manifolds_hom.txt  \
+          > 3_manifolds.json
+
+gzip --best 3_manifolds.json
