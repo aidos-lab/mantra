45c987dbef03bb24861d7acce177b031e3959424
Ernst Roell <ernstroell@gmail.com>
2024-07-25 09:55:32 +0200

    Added rdt theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,39 +6,40 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'MANTRA'
-copyright = '2024, Ernst Röell, Daniel Bin Schmid, Bastian Rieck'
-author = 'Ernst Röell, Daniel Bin Schmid, Bastian Rieck'
+project = "MANTRA"
+copyright = "2024, Ernst Röell, Daniel Bin Schmid, Bastian Rieck"
+author = "Ernst Röell, Daniel Bin Schmid, Bastian Rieck"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
+    "sphinx_rtd_theme",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
 
 
 # Ensure that member functions are documented. These are sane defaults.
 autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource',
-    'special-members': '__init__',
-    'undoc-members': True,
-    'exclude-members': '__weakref__'
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
 }
 
 # Tries to assign some semantic meaning to arguments provided with
 # single backtics, such as `x`. This way, we can ignore `func` and
 # `class` targets etc. (They still work, though!)
-default_role = 'obj'
+default_role = "obj"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
-modindex_common_prefix = ['mantra.']
+modindex_common_prefix = ["mantra."]
