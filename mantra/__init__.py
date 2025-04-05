7bd6e3c9c2171b967e20f942d83056afb38ac78d
Rubén Ballester <rballeba@gmail.com>
2025-04-05 20:21:41 +0200

    Adding some changes to the initialization module and on the tutorials.

---
+++ mantra/__init__.py
@@ -0,0 +1,15 @@
+from mantra.transforms import (
+    NodeIndex,
+    RandomNodeFeatures,
+    DegreeTransform,
+    DegreeTransformOneHot,
+)
+from mantra.datasets import ManifoldTriangulations
+
+__all__ = [
+    'NodeIndex',
+    'RandomNodeFeatures',
+    'DegreeTransform',
+    'DegreeTransformOneHot',
+    'ManifoldTriangulations',
+]
