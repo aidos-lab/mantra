0a1a42966b0c6a86e287b1d16b0dadd5d2c0e553
Bastian Rieck <bastian@rieck.me>
2025-11-14 14:54:29 +0100

    Preparing some simple statistical graphics
    This is currently just for the 2D manifolds but at least we get to
    experience the data somewhat more nicely.

---
+++ scripts/calculate_statistics.py
@@ -0,0 +1,34 @@
+import json
+import sys
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as f:
+        data = json.load(f)
+
+    df = pd.DataFrame.from_dict(data).set_index("id")
+    df = df.drop(
+        columns=[
+            "triangulation",
+            "torsion_coefficients",
+            "betti_numbers",
+            "orientable",
+            "dimension",
+        ]
+    )
+
+    # So sue me, right?
+    interesting_manifolds = ["RP^2", "T^2", "Klein bottle", "S^2"]
+
+    df = df.query("name in @interesting_manifolds")
+    df = df.groupby(["name", "n_vertices"]).size().to_frame("count")
+
+    sns.set_theme(style="white", palette="Set1")
+
+    sns.lineplot(df, x="n_vertices", y="count", hue="name")
+    plt.tight_layout()
+    plt.show()
