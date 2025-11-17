74fca14caf37b49f8983caae749627debc101c1c
Martin Carrasco <martin.carrascocastaneda@unifr.ch>
2025-11-17 15:11:03 +0100

    Base refactor

---
+++ mantra/tasks/task_types.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class TaskType(Enum):
+    NAME = "name"
+    ORIENTABILITY = "orientability"
+    BETTI_NUMBERS = "betti_numbers"
+    HOMEOMORPHIC_DIST = "homeomorphic_dist"
+
