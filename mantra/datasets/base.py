8c9c5b29d0f1e7352ae941d4dd304f2e20e4800a
Bastian Rieck <bastian@rieck.me>
2025-11-19 10:27:20 +0100

    Simplified `get_url()`

--- mantra/datasets/base.py
+++ mantra/datasets/base.py
@@ -17,31 +17,29 @@ from torch_geometric.data import (
 
 
 def get_url(version: str, manifold: str) -> str:
-    """
-    Function to fetch the full download url.
-    """
+    """Get URL to download dataset from."""
     if version == "latest":
         return f"https://github.com/aidos-lab/MANTRA/releases/latest/download/{manifold}_manifolds.json.gz"  # noqa
 
-    # Set headers for GitHub API
     headers = {
         "Accept": "application/vnd.github+json",
         "X-GitHub-Api-Version": "2022-11-28",
     }
 
-    # Fetch the list of versions.
     response = requests.get(
         "https://api.github.com/repos/aidos-lab/mantra/releases",
         headers=headers,
     )
+
     all_available_versions = [item["name"] for item in response.json()]
+
     if version not in all_available_versions:
         raise ValueError(
             f"Version {version} not available, please choose one of the following versions: {all_available_versions}."  # noqa
         )
 
-    # Note that the url order is different (inconsistent) for a
-    # specific release as compared to "latest".
+    # Note that the URL order is different and thus inconsistent for a
+    # specific release.
     return f"https://github.com/aidos-lab/MANTRA/releases/download/{version}/{manifold}_manifolds.json.gz"  # noqa
 
 
