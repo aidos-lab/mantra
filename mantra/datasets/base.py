65a20a4d936fee893d758b890f9eaa5eb1ba8036
Bastian Rieck <bastian@rieck.me>
2025-11-19 10:36:03 +0100

    Improved documentation of base dataset class

--- mantra/datasets/base.py
+++ mantra/datasets/base.py
@@ -56,15 +56,16 @@ class ManifoldTriangulations(InMemoryDataset):
         force_reload=False,
     ):
         """
-        The dataset class for the manifold triangulations.
+        Load new dataset of manifold triangulations.
 
         Parameters
         ----------
-        manifold: string
-            Wether to use the 2- or 3- manifolds. The 2-manifold dataset
-            consist of all surfaces with up to 10 vertices. The
-            3-manifolds consist of all triangulatoins with up to 10 vertices.
-        version: string
+        manifold : string
+            Dimension of manifolds to load. Currently, only "2" or "3"
+            are supported, denoting surfaces and 3-manifolds,
+            respectively.
+
+        version : string
             Version of the dataset to use. The version should correspond to a
             released version of the dataset, all of which can be found
             `on GitHub <https://github.com/aidos-lab/mantra/releases>`__.
@@ -72,14 +73,7 @@ class ManifoldTriangulations(InMemoryDataset):
             specific reproducibility requirements are to be met, using
             `latest` is recommended.
         """
-        # The properties need to be set before the super().__init__() call to
-        # make sure they exist during processing. The process and download are
-        # called during the super call.
-
-        if manifold not in ["2", "3"]:
-            raise ValueError(
-                f"Manifolds should either be 2 or 3, you provided {manifold}"
-            )
+        assert manifold in ["2", "3"]
 
         self.manifold = manifold
         self.version = version
@@ -102,42 +96,31 @@ class ManifoldTriangulations(InMemoryDataset):
 
     @property
     def raw_file_names(self):
+        """Return raw file names.
+
+        Stores the raw file names that need to be present in the raw folder
+        for downloading to be skipped. To reference raw file names, use the
+        property `self.raw_paths`.
         """
-        Stores the raw  file names that need to be present in the raw folder for
-        downloading to be skipped. To reference raw file names, use the property
-        self.raw_paths.
-        """
-        return [
-            f"{self.manifold}_manifolds.json",
-        ]
+        return [f"{self.manifold}_manifolds.json"]
 
     @property
     def processed_file_names(self):
-        """
-        Stores the processed data in a file, if this file is present in the
-        processed folder, it will skip processing. Othewise it will run the
-        process function.
+        """Return process file names.
+
+        Stores the processed data in a file. If this file is present in the
+        `processed` folder, processing will typically be skipped.
         """
         return [f"data_{self.manifold}.pt"]
 
     def download(self) -> None:
-        """
-        Downloads the specified version of the 2 or 3 manifolds in json format
-        into the raw folder and extracts the results. The dataset version can
-        specified when instantiating the class.
-        """
+        """Download dataset depending on specified version."""
         path = download_url(self.url, self.raw_dir)
         extract_gz(path, self.raw_dir)
         os.unlink(path)
 
     def process(self):
-        """
-        Processes the raw json file and loads the result into a torch-geometric
-        dataset. If provided during initialization, pretransforms and/or
-        prefilters are applied before saving the preprocessed dataset.
-        More information on pretransforms and prefilters can be found in the
-        `pytorch-geometric documentation <https://pytorch-geometric.readthedocs.io/en/latest/modules/transforms.html>`__.
-        """
+        """Processes dataset."""
         with open(self.raw_paths[0]) as f:
             inputs = json.load(f)
 
