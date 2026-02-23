e88d261e6f9f30fd47297b026d330213d351a3ad
Inés García-Redondo <ines.garciaredondo@unifr.ch>
2026-02-23 13:15:07 +0100

    added encoding as statistics

--- mantra/transforms/__init__.py
+++ mantra/transforms/__init__.py
@@ -3,6 +3,7 @@ from .select_attributes import SelectAttributes
 from .create_labels import CreateLabels
 from .select_features import SelectFeatures
 from .attribute_transform import NodeDegreeTransform, NodeRandomTransform
+from .effective_resistance import EffectiveResistanceEmbedding, EffectiveResistanceStatisticsEmbedding
 
 __all__ = [
     "CreateLabels",
@@ -11,4 +12,6 @@ __all__ = [
     "SelectFeatures",
     "NodeRandomTransform",
     "NodeDegreeTransform",
+    "EffectiveResistanceEmbedding",
+    "EffectiveResistanceStatisticsEmbedding"
 ]
