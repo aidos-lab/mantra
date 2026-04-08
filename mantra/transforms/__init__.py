c527cfbeaad209fc95f40cdfb289608255d98cac
Martin Carrasco <martin.carrascocastaneda@unifr.ch>
2026-04-08 10:15:18 +0200

    FIX Lint again

--- mantra/transforms/__init__.py
+++ mantra/transforms/__init__.py
@@ -1,14 +1,13 @@
+from .attribute_transform import (
+    NodeDegreeTransform,
+    NodeIndex,
+    NodeRandomTransform,
+    RandomNodeFeatures,
+)
 from .create_labels import CreateLabels
 from .moment_curve_embedding import MomentCurveEmbedding
 from .select_attributes import SelectAttributes
-from .create_labels import CreateLabels
 from .select_features import SelectFeatures
-from .attribute_transform import (
-    NodeDegreeTransform,
-    NodeRandomTransform,
-    NodeIndex,
-    RandomNodeFeatures,
-)
 
 __all__ = [
     "CreateLabels",
