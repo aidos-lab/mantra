ab561d41231a9c529e0e0dfb2666d89a135b635d
Martin Carrasco <martin.carrascocastaneda@unifr.ch>
2026-06-19 14:10:47 +0200

    FIX: Another lint

--- mantra/representations/__init__.py
+++ mantra/representations/__init__.py
@@ -1,7 +1,7 @@
 from .dual_graph import DualGraph
 from .hasse_diagram import HasseDiagram
-from .one_skeleton import OneSkeleton
 from .levi_graph import LeviGraph
+from .one_skeleton import OneSkeleton
 from .simplicial_connectivity import (
     AdjacencySimplicialComplex,
     CoadjacencySimplicialComplex,
