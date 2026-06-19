f48634de9e20b4cfc5e8944440f06b3e518cccea
Martin Carrasco <martin.carrascocastaneda@unifr.ch>
2026-06-19 13:09:10 +0200

    FIX: Replace previous transform with correct implementation for chaining simplex features

--- mantra/transforms/attribute_transform.py
+++ mantra/transforms/attribute_transform.py
@@ -5,9 +5,48 @@ transformations in `our paper <https://openreview.net/pdf?id=X6y5CC44HM>`__
 to enable the training on different neural-network architectures.
 """
 
+import math
+
 import torch
 import torch_geometric.transforms as T
 from torch_geometric.utils import degree
+
+
+class SimplexRandomTransform(T.BaseTransform):
+    """Add random features to `simplex_dim` simplices
+    with a `feature_dim` dimension.
+
+    We check the triangulation to derive the number of
+    `simplex_dim` dimensional simplices.
+    """
+
+    def __init__(self, simplex_dim: int, feature_dim: int = 8):
+        super().__init__()
+        self.featur_dim = feature_dim
+        self.k = simplex_dim
+
+    def forward(self, data):
+        assert "triangulation" in data, "Field 'triangulation` not found"
+
+        top_simps = set([tuple(s) for s in data.triangulation])
+        k_dim_simps = 0
+
+        # For each top-simplex we count the simplices that need to exists
+        # due to the closure property, we just count for each
+        # NOTE: If we assume that all top-level simplices have the same
+        # dimension we could just to len(top_simps) * math.comb(n,k)
+        for top_simp in top_simps:
+            k_dim_simps += math.comb(n=len(top_simp), k=self.k)
+
+        # Create tensor on float32
+        feat_tensor = torch.rand(
+            size=(k_dim_simps, self.featur_dim), dtype=torch.float32
+        )
+
+        # Set tensor
+        setattr(data, f"random_features_{self.k}", feat_tensor)
+
+        return data
 
 
 class NodeRandomTransform(T.BaseTransform):
@@ -15,49 +54,15 @@ class NodeRandomTransform(T.BaseTransform):
     Add random node features in `random_features`
     """
 
-    def __init__(self, dim: int = 8, propagate: bool = False):
+    def __init__(self, dim: int = 8):
         super().__init__()
         self.dimension = dim
-        self.propagate = propagate
 
     def forward(self, data):
-        if not self.propagate:
-            assert "edge_index" in data, "No edge index in data"
-            data.random_features = torch.rand(
-                size=(int(data.edge_index.max().item() + 1), self.dimension)
-            )
-        else:  # Propagate random features to all simplices
-            # All incidence matrices required
-            incidence_list = [k for k in data.keys() if "incidence" in k]
-
-            assert (
-                len(incidence_list) > 0
-            ), "No incidence matrices found in data"
-
-            # Sort by rank `r`
-            incidence_list = sorted(
-                incidence_list, key=lambda x: int(x.split("_")[1])
-            )
-
-            random_features = {}
-
-            for inc_m in incidence_list:
-                incidence_matrix = getattr(data, inc_m)
-                r_to = int(inc_m.split("_")[1])
-                r_from = r_to - 1
-                # Case for incidence_0
-                if r_from < 0:
-                    continue
-
-                random_features[r_from] = torch.rand(
-                    size=(incidence_matrix.shape[0], self.dimension)
-                )
-
-                random_features[r_to] = torch.rand(
-                    size=(incidence_matrix.shape[1], self.dimension)
-                )
-            data.random_features = random_features
-        return data
+        assert "edge_index" in data, "No edge index in data"
+        data.random_features = torch.rand(
+            size=(int(data.edge_index.max().item() + 1), self.dimension)
+        )
 
 
 class NodeDegreeTransform(T.BaseTransform):
