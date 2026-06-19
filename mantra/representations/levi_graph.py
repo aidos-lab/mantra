2fcb62aeab73cf486dc7539542b618d490503680
Martin Carrasco <martin.carrascocastaneda@unifr.ch>
2026-06-19 14:08:31 +0200

    Linting!

--- mantra/representations/levi_graph.py
+++ mantra/representations/levi_graph.py
@@ -13,8 +13,8 @@ class LeviGraph(BaseTransform):
         if it's interpreted as a `configuration` between 0-simplices
         and maximal simplices.
 
-        [1] Hauschild, J., Ortiz, J., & Vega, O. (2015). On the Levi graph of point-line configurations. Involve, a Journal of Mathematics, 8(5), 893-900. 
-        https://msp.org/involve/2015/8-5/involve-v8-n5-p14-s.pdf 
+        [1] Hauschild, J., Ortiz, J., & Vega, O. (2015). On the Levi graph of point-line configurations. Involve, a Journal of Mathematics, 8(5), 893-900.
+        https://msp.org/involve/2015/8-5/involve-v8-n5-p14-s.pdf
 
         .
 
@@ -46,7 +46,7 @@ class LeviGraph(BaseTransform):
 
         The Levi Graph of a triangulation $T$ of dimension $d$ is a graph
         that has 0-simplices and k-simplices as
-        nodes and each 0-simplex is connected to a k-simplex if 
+        nodes and each 0-simplex is connected to a k-simplex if
         it's contained in it.
 
         """
@@ -55,7 +55,6 @@ class LeviGraph(BaseTransform):
 
         # Number of maximal simplices
         m = len(top_simplices)
-
 
         # Collect all the nodes
         # composing the simplices
@@ -70,9 +69,9 @@ class LeviGraph(BaseTransform):
 
         # For each maximal simplex
         for i, simp in enumerate(top_simplices):
-            G.add_node(n+i, simplex=simp)
+            G.add_node(n + i, simplex=simp)
             # Add edges
             for node in simp:
-                G.add_edge(node-1, n+i)
+                G.add_edge(node - 1, n + i)
 
         return G
