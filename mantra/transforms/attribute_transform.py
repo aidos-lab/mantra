2fc08d242538c980982130012ab31d86a55e9b38
Martin Carrasco <martin.carrascocastaneda@unifr.ch>
2026-07-08 18:28:35 +0200

    Cleanup of bad merge

--- mantra/transforms/attribute_transform.py
+++ mantra/transforms/attribute_transform.py
@@ -61,45 +61,12 @@ class NodeRandomTransform(T.BaseTransform):
     def __init__(self, dim: int = 8):
         super().__init__()
         self.dimension = dim
-        self.propagate = propagate
 
     def forward(self, data):
-        if not self.propagate:
-            assert "edge_index" in data, "No edge index in data"
-            data.random_features = torch.rand(
-                size=(int(data.edge_index.max().item() + 1), self.dimension)
-            )
-        else:  # Propagate random features to all simplices
-            # All incidence matrices required
-            incidence_list = [k for k in data.keys() if "incidence" in k]
-
-            assert (
-                len(incidence_list) > 0
-            ), "No incidence matrices found in data"
-
-            # Sort by rank `r`
-            incidence_list = sorted(
-                incidence_list, key=lambda x: int(x.split("_")[1])
-            )
-
-            random_features = {}
-
-            for inc_m in incidence_list:
-                incidence_matrix = getattr(data, inc_m)
-                r_to = int(inc_m.split("_")[1])
-                r_from = r_to - 1
-                # Case for incidence_0
-                if r_from < 0:
-                    continue
-
-                random_features[r_from] = torch.rand(
-                    size=(incidence_matrix.shape[0], self.dimension)
-                )
-
-                random_features[r_to] = torch.rand(
-                    size=(incidence_matrix.shape[1], self.dimension)
-                )
-            data.random_features = random_features
+        assert "edge_index" in data, "No edge index in data"
+        data.random_features = torch.rand(
+            size=(int(data.edge_index.max().item() + 1), self.dimension)
+        )
         return data
 
 
