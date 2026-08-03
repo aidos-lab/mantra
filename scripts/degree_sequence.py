39a874b99a80f37d2fb9c3419f5ec40aa31a17dc
Bastian Rieck <bastian@rieck.me>
2026-08-03 17:51:39 +0200

    More plotting

--- scripts/degree_sequence.py
+++ scripts/degree_sequence.py
@@ -3,9 +3,18 @@ import sys
 
 import numpy as np
 import networkx as nx
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from itertools import combinations
+
+from scipy.cluster.hierarchy import linkage
+from scipy.spatial.distance import squareform
 from scipy.stats import wasserstein_distance
+
+from sklearn.metrics import silhouette_samples
 
 
 def pairwise_wasserstein(sequences):
@@ -28,11 +37,42 @@ if __name__ == "__main__":
         data = json.load(f)
 
         degree_sequences = []
+        labels = []
 
-        for manifold in data[:50]:
+        for manifold in data:
+            if manifold["name"] not in ["S^2", "T^2", "RP^2", "Klein bottle"]:
+                continue
+
+            if len(degree_sequences) == 100:
+                break
+
             G = one_skeleton(manifold["triangulation"])
+
             degree_sequence = sorted((d for _, d in G.degree()), reverse=True)
             degree_sequences.append(degree_sequence)
 
+            labels.append(manifold["name"])
+
         D = pairwise_wasserstein(degree_sequences)
-        print(D)
+
+        sil = silhouette_samples(D, labels, metric="precomputed")
+        df = pd.DataFrame({"label": labels, "silhouette": sil})
+
+        print(df.groupby("label")["silhouette"].mean().sort_values())
+
+        print(sil)
+
+        C = squareform(D, checks=False)
+        Z = linkage(C, method="average")
+
+        sns.clustermap(
+            D,
+            row_linkage=Z,
+            col_linkage=Z,
+            xticklabels=labels,
+            yticklabels=labels,
+            annot=True,
+            fmt=".2f",
+        )
+
+        plt.show()
