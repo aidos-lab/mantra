0e7aebb2a5a105df6c5a3a2b350e68cf9ab4afa9
Bastian Rieck <bastian@rieck.me>
2026-08-12 18:15:51 +0200

    Preparing f-vector calculation and matrix representation

---
+++ scripts/f_vector.py
@@ -0,0 +1,88 @@
+import math
+import json
+import random
+import sys
+
+import numpy as np
+
+from itertools import combinations
+from itertools import permutations
+
+from scipy.special import stirling2
+
+
+def faces(top_simplices):
+    F = set()
+
+    for s in top_simplices:
+        s = tuple(sorted(s))
+        for k in range(1, len(s) + 1):
+            F.update(combinations(s, k))
+
+    return F
+
+
+def f_vector(top_simplices):
+    count = {}
+
+    for f in faces(top_simplices):
+        count[len(f) - 1] = count.get(len(f) - 1, 0) + 1
+
+    return [count.get(i, 0) for i in range(max(count) + 1)]
+
+
+def barycentric_subdivision(top_simplices):
+    new = set()
+
+    for s in top_simplices:
+        s = tuple(sorted(s))
+        for p in permutations(s):
+            new.add(
+                tuple(
+                    sorted(tuple(sorted(p[:k])) for k in range(1, len(p) + 1))
+                )
+            )
+
+    return sorted(new)
+
+
+def euler_characteristic(top_simplices):
+    return sum((-1) ** (len(f) - 1) for f in faces(top_simplices))
+
+
+def brenti_welker_matrix(dim):
+    d = dim + 1
+
+    return np.asarray(
+        [
+            [math.factorial(i + 1) * stirling2(j, i + 1) for j in range(d)]
+            for i in range(d)
+        ],
+        float,
+    )
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as f:
+        data = json.load(f)
+        data = list(
+            filter(
+                lambda manifold: manifold["name"]
+                in ["Klein bottle", "RP^2", "S^2", "T^2"],
+                data,
+            )
+        )
+
+    data = random.sample(data, 500)
+
+    for manifold in data:
+        K = manifold["triangulation"]
+        L = barycentric_subdivision(K)
+
+        print(
+            manifold["name"],
+            f_vector(K),
+            euler_characteristic(K),
+            f_vector(L),
+            euler_characteristic(L),
+        )
